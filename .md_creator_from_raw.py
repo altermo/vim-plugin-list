125860c4fc2b1fb8062feffcdb211b6bad9dcca9
altermo <dagany.tamas@gmail.com>
2022-06-21 08:14:16 +0200

    M .md_creator_from_raw.py

--- .md_creator_from_raw.py
+++ .md_creator_from_raw.py
@@ -5,9 +5,7 @@ def main():
 This is a list of plugins.
 _TODO: categorize, document and remove not plugins_
 
-_NOTE: this list may contain mirrors._
-
-_NOTE: this list may contain:extensions to plugins, links that are not working and other things that are not related to vim plugins._
+_NOTE: this list may contain: mirrors, extensions to plugins, links that are not working and other things that are not related to vim plugins._
 
 _Other vim plugin lists: [awesome-vim](https://github.com/akrawchyk/awesome-vim), [neovim-official-list](https://github.com/neovim/neovim/wiki/Related-projects#plugins)_
 '''
